ee3419da91b8c2ddf1150cd694a2c2d8d09f93b2
Kristel Maximova <idmaksimovak@gmail.com>
2023-10-26 16:56:41 +0300

    Initial Commit

---
+++ Multistore/MultistoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultistoreApp.swift
+//  Multistore
+//
+//  Created by Kristi on 26.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct MultistoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
